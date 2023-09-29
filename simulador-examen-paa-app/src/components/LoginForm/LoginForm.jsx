154d1948a23ef30c9b96a0fbe14fd4211e48e105
JorgeML01 <jorgedd2001@gmail.com>
2023-09-29 15:54:57 -0600

    comments

--- simulador-examen-paa-app/src/components/LoginForm/LoginForm.jsx
+++ simulador-examen-paa-app/src/components/LoginForm/LoginForm.jsx
@@ -17,12 +17,12 @@ import {
 function LoginForm() {
   const navigate = useNavigate();
   const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
+  //! const [isSubmitted, setIsSubmitted] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleLoginSuccess = () => {
-    setIsSubmitted(true);
+    //! setIsSubmitted(true);
     navigate("/");
     window.location.reload();
   };
