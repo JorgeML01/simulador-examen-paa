a4b7a6a3785bf4f3107bc36fc4ae1f2bc6b9ea2c
JorgeML01 <jorgedd2001@gmail.com>
2023-09-30 19:29:30 -0600

    navbar updated

--- simulador-examen-paa-api/src/middlewares/middleware.js
+++ simulador-examen-paa-api/src/middlewares/middleware.js
@@ -1,6 +1,13 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+/**
+ * Middleware function to authenticate a token in the request header.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {Object} - The response object with a status code and message if the token is invalid or missing, otherwise calls the next middleware function.
+ */
 function authenticateToken(req, res, next) {
   const token = req.headers["authorization"];
 
--- simulador-examen-paa-app/src/components/Navbar/Navbar.jsx
+++ simulador-examen-paa-app/src/components/Navbar/Navbar.jsx
@@ -3,32 +3,45 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import "./styles.css";
 
 function NavigationBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      className="bg-black bg-gradient font-size-nav"
+    >
       <Container>
-        <Navbar.Brand href="/">Simulador Admisiones UNAH</Navbar.Brand>
+        <Navbar.Brand href="/" className="color-container-nav">
+          Simulador Admisiones UNAH
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/ranking">Ranking</Nav.Link>
-            <Nav.Link href="/carreras">Carreras</Nav.Link>
-            <NavDropdown title="Simuladores" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="/PAA">Prueba PAA</NavDropdown.Item>
-              <NavDropdown.Item href="/PAM">Prueba PAM</NavDropdown.Item>
-              <NavDropdown.Item href="/PCCNS">Prueba PCCNS</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/informacion">
-                Información
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="/ranking" className="color-container-nav">
+              Ranking
+            </Nav.Link>
+            <Nav.Link href="/carreras" className="color-container-nav">
+              Carreras
+            </Nav.Link>
+            <Nav.Link href="/simuladores" className="color-container-nav">
+              Simuladores
+            </Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link eventKey={2} href="/login">
+            <Nav.Link
+              eventKey={2}
+              href="/login"
+              className="color-container-nav"
+            >
               Iniciar Sesión
             </Nav.Link>
-            <Nav.Link eventKey={2} href="/registro">
+            <Nav.Link
+              eventKey={2}
+              href="/registro"
+              className="color-container-nav"
+            >
               Registrarse
             </Nav.Link>
           </Nav>
