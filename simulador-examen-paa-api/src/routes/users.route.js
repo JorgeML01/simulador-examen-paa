4ba423492ac6d0fd7f149e678dc5a0f6e720cb71
JorgeML01 <jorgedd2001@gmail.com>
2023-10-01 01:32:58 -0600

    login middleware

--- simulador-examen-paa-api/src/routes/users.route.js
+++ simulador-examen-paa-api/src/routes/users.route.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const UserController = require("../controllers/users.controller");
+const { authenticateToken } = require("../middlewares/middleware");
 
 router.post("/register", UserController.register);
-router.post("/login", UserController.login);
+router.post("/login", authenticateToken, UserController.login);
 
 module.exports = router;
