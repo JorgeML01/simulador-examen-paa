16d9daabfa7789f64a98fcefd4e010cbae4500f7
JorgeML01 <jorgedd2001@gmail.com>
2023-10-02 15:48:57 -0600

    status res

--- simulador-examen-paa-api/src/middlewares/middleware.js
+++ simulador-examen-paa-api/src/middlewares/middleware.js
@@ -13,12 +13,12 @@ function authenticateToken(req, res, next) {
   const token = req.headers["authorization"];
   console.log(token);
 
-  if (token == null) return res.sendStatus(401).send("Unauthorized");
+  if (token == null) return res.status(401).send("Unauthorized");
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) {
       console.log(err);
-      return res.sendStatus(403).send("Invalid token");
+      return res.status(403).send("Invalid token");
     } else {
       req.user = user;
       next();
