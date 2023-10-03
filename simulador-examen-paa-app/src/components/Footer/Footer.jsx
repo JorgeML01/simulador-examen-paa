8138583b5f5869c408650851067e919010fc6614
JorgeML01 <jorgedd2001@gmail.com>
2023-10-02 21:06:51 -0600

    href fixed - footer

--- simulador-examen-paa-app/src/components/Footer/Footer.jsx
+++ simulador-examen-paa-app/src/components/Footer/Footer.jsx
@@ -51,22 +51,22 @@ function MainFooter() {
             <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
               <h6 className="text-uppercase fw-bold mb-4">Products</h6>
               <p>
-                <a href="#!" className="text-reset">
+                <a href="/" className="text-reset">
                   Angular
                 </a>
               </p>
               <p>
-                <a href="#!" className="text-reset">
+                <a href="/" className="text-reset">
                   React
                 </a>
               </p>
               <p>
-                <a href="#!" className="text-reset">
+                <a href="/" className="text-reset">
                   Vue
                 </a>
               </p>
               <p>
-                <a href="#!" className="text-reset">
+                <a href="/" className="text-reset">
                   Laravel
                 </a>
               </p>
@@ -75,22 +75,22 @@ function MainFooter() {
             <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
               <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
               <p>
-                <a href="#!" className="text-reset">
+                <a href="/" className="text-reset">
                   Pricing
                 </a>
               </p>
               <p>
-                <a href="#!" className="text-reset">
+                <a href="/" className="text-reset">
                   Settings
                 </a>
               </p>
               <p>
-                <a href="#!" className="text-reset">
+                <a href="/" className="text-reset">
                   Orders
                 </a>
               </p>
               <p>
-                <a href="#!" className="text-reset">
+                <a href="/" className="text-reset">
                   Help
                 </a>
               </p>
